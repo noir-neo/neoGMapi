1e46664d2c573cec9760b4d11769b666aaa274aa
neo-PC <noir.neo.04@gmail.com>
2014-03-06 11:15:50 +0900

    update main.js comments

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
+// ゲーム開始時に呼んでください。
 ngm.gameStart();
 
+// hiscoreを取得します。
+// 今までPOSTしたscoreすべての中の最高値です。
+console.log('hiscore:'+ngm.getHiscore());
+
+// gamelevelをGETします。callback関数を引数にしてください。
 ngm.getGamelevel(function(gamelevel) {
   console.log('gamelebel:'+gamelevel);
 });
-console.log('hiscore:'+ngm.getHiscore());
 
+// レベルプレイ開始ごとに呼んでください。POSTする時間を保持しています。
 ngm.stageStart();
 
-
-
 setInterval(function() {
+  
+  // クリア結果をPOSTします。
   ngm.stageResult({
-  gamelevel:5,
-  score:1073741823,
-  getbadgeid: 2});
+    gamelevel:10,// クリアしたレベル
+    score:114514, // 取得したスコア
+    getbadgeid: 0, // クリアによって取得したバッジがあればIDを
+    // なければnullなり空配列[]なり''なり適当に入れてください。
+    // 1回のクリアで複数バッジを取得した場合は配列で
+    // getbadgeid: [0,2],
+    });
 
 }, 10000); 
